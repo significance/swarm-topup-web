d128a7ad51e0c4804c6e22a7efd350867644fe68
sig <dan@1up.digital>
2022-05-03 10:17:19 +0200

    z < s

--- src/components/calculate-cost.tsx
+++ src/components/calculate-cost.tsx
@@ -2,18 +2,18 @@ type CalculateCostProps = {
 	amount: number
 }
 
-const BUCKET_CHUNKZ = 524288 //2^19 since 19 is the bucket depth
-const BZZ_DECIMALZ_INVERTED = 10000000000000000
+const BUCKET_CHUNKS = 524288 //2^19 since 19 is the bucket depth
+const BZZ_DECIMALS_INVERTED = 10000000000000000
 
 export const CalculateCost = ({ amount }: CalculateCostProps) => {
 	return (
 		<div>
 			<ul>
 				<li>
-					{amount * BUCKET_CHUNKZ} <em>plur</em>
+					{amount * BUCKET_CHUNKS} <em>plur</em>
 				</li>
 				<li>
-					{(amount * BUCKET_CHUNKZ) / BZZ_DECIMALZ_INVERTED}{' '}
+					{(amount * BUCKET_CHUNKS) / BZZ_DECIMALS_INVERTED}{' '}
 					<strong>BZZ</strong>
 				</li>
 			</ul>
